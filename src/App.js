46bd6daf7c990d239800d9f7f975eead4b3f8bd1
Yummy275 <htgonzalez94@yahoo.com>
2021-06-20 16:25:24 -0700

    qs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { useInView } from 'react-intersection-observer';
+
+const ScrollWrapper = styled.div`
+    height: 50rem;
+    background-color: ${(props) => (props.inView ? 'red' : 'green')};
+`;
 
 function App() {
-    return <div className="App"> Hello</div>;
+    const [test, inView] = useInView({
+        threshold: 0.2,
+    });
+
+    return (
+        <div className="App">
+            <div style={{ height: '100vh', backgroundColor: 'blue' }}></div>
+            <ScrollWrapper ref={test} inView={inView}></ScrollWrapper>
+        </div>
+    );
 }
 
 export default App;
