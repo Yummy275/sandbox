ce02381c8ba2668cc5b8e5355649cd8a25c65562
Yummy275 <htgonzalez94@yahoo.com>
2021-06-20 17:18:10 -0700

    qs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useInView } from 'react-intersection-observer';
+import ScrollAction from './components/ScrollAction';
+import TestCompTwo from './components/TestCompTwo';
 
 const ScrollWrapper = styled.div`
     height: 50rem;
-    background-color: ${(props) => (props.inView ? 'red' : 'green')};
+    background-color: black;
 `;
 
 function App() {
@@ -12,10 +14,21 @@ function App() {
         threshold: 0.2,
     });
 
+    const [testTwo, inViewTop] = useInView({
+        threshold: 0.5,
+    });
+
     return (
         <div className="App">
-            <div style={{ height: '100vh', backgroundColor: 'blue' }}></div>
-            <ScrollWrapper ref={test} inView={inView}></ScrollWrapper>
+            <TestCompTwo visible={inViewTop}>
+                <div
+                    ref={testTwo}
+                    style={{ height: '100vh', backgroundColor: 'blue' }}
+                ></div>
+            </TestCompTwo>
+            <ScrollWrapper ref={test}>
+                <ScrollAction appear={inView}></ScrollAction>
+            </ScrollWrapper>
         </div>
     );
 }
--- src/components/ScrollAction.js
+++ src/components/ScrollAction.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import greenPlanetImg from '../assets/green-planet.png';
 
-const ScrollAction = () => {
+const FadeIn = keyframes`
+    from {
+        opacity: 0;
+        transform: translate(20px, 20px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translate(0);
+    }
+`;
+
+const GreenPlanet = styled.img`
+    opacity: 0;
+    animation: ${FadeIn} 0.3s ease forwards;
+`;
+
+const ScrollAction = ({ appear }) => {
     return (
-        <div>
-            <div></div>
-        </div>
+        <>{appear ? <GreenPlanet src={greenPlanetImg}></GreenPlanet> : ''}</>
     );
 };
 
